67cc0a69c0f353e14ec6111bc0b9dbde1d347be4
Dan Schultz <slifty@gmail.com>
2024-02-02 10:47:39 -0500

    Add february pool / participants

--- src/data/itemPools.ts
+++ src/data/itemPools.ts
@@ -260,4 +260,30 @@ export const itemPools: Record<string, ItemPool> = {
 			},
 		],
 	},
+	"2024-02": {
+		standard: [
+			{
+				name: "mummified entombed cookbookbat",
+				quantity: 1,
+			},
+			{
+				name: "combat lover's locket lockbox",
+				quantity: 1,
+			},
+		],
+		legacy: [
+			{
+				name: "Buddy Bjorn",
+				quantity: 1,
+			},
+			{
+				name: "corked genie bottle",
+				quantity: 1,
+			},
+			{
+				name: "shortest-order cook",
+				quantity: 1,
+			},
+		],
+	},
 };
